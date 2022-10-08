33c84ac038fe1407b404f8886c4d5b9e22b8221d
duy0299 <baoduy0299@gmail.com>
2022-10-08 23:47:14 +0700

    Start project. source java and set properties

---
+++ src/test/java/com/Rookies/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rookies.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
